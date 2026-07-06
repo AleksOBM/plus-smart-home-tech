06b4f09fdf38c24446f689b66203ec2a45cdf4c5
AleksOBM <obmetkin.a.v@yandex.ru>
2026-07-06 19:44:25 +0300

    Добавлен discovery-server

--- telemetry/hub-router/src/main/java/ru/yandex/practicum/telemetry/hub_router/config/HubRouterConfiguration.java
+++
@@ -1,8 +0,0 @@
-package ru.yandex.practicum.telemetry.hub_router.config;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class HubRouterConfiguration {
-
-}
