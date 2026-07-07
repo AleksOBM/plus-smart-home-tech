3b45aaaea92fc4cf95e56f9eeb483b240b9c089b
AleksOBM <obmetkin.a.v@yandex.ru>
2026-07-07 19:56:22 +0300

    Добавлено автоопределение ip-адресов

--- telemetry/collector/src/main/java/ru/yandex/practicum/telemetry/collector/CollectorApp.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/telemetry/collector/CollectorApp.java
@@ -2,6 +2,7 @@ package ru.yandex.practicum.telemetry.collector;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 public class CollectorApp {
